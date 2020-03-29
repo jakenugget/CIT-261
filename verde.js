4a6a0a3b9f73f20940ece6620771dc489336ecdc
jakenugget <59673380+jakenugget@users.noreply.github.com>
2020-03-29 04:39:30 -0600

    Update verde.js

--- verde.js
+++ verde.js
@@ -62,3 +62,10 @@ window.onload = function() {
     localStorage.removeItem("toDoList2");
   };
 };
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  });
+}
